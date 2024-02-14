874c9e4878e1aec8d64a9b7e6facadb9a3fd02de
Turg0n <turgon1661@gmail.com>
2024-02-14 13:51:37 +0200

    bar-finish

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,52 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const form = document.querySelector('form');
+const numberInput = document.querySelector('[type=number]');
+
+form.addEventListener('submit', onSubmitBtnClick);
+
+function onSubmitBtnClick(e) {
+    e.preventDefault();
+
+    const makePromise = () => {
+    return new Promise((resolve, reject) => {
+        const delay = numberInput.value;
+        const shouldResolve = form.elements.state.value;
+        
+        setTimeout(() => {
+        if (shouldResolve === 'fulfilled') {
+            resolve(`✅ Fulfilled promise in ${delay}ms`);
+        } else {
+            reject(`❌ Rejected promise in ${delay}ms`);
+        }
+        }, delay);
+    });
+    };
+
+    makePromise()
+    .then(value =>
+        iziToast.show({
+        message: value,
+        position: 'topRight',
+        backgroundColor: 'green',
+        messageColor: 'white',
+        close: false,
+        progressBar: false,
+        class: '.snackbar-izitoast',
+        })
+    )
+    .catch(error =>
+        iziToast.show({
+        message: error,
+        position: 'topRight',
+        backgroundColor: 'red',
+        messageColor: 'white',
+        close: false,
+        progressBar: false,
+        class: '.snackbar-izitoast',
+        })
+    );
+
+form.reset();
+};
+
